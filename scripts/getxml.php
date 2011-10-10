630e1645d8588cde42dc365a178a4e4a9609ed43
ZH <zh199111@gmail.com>
2011-10-10 17:41:37 +0800

    test

---
+++ scripts/getxml.php
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0">
+  <channel>
+    <title>Google Logos</title>
+    <link>http://www.google.com/intl/en_us/logos/index.html</link>
+    <description>Holiday Logos and Events - Google style! A collection of all Google logos
+    commemorating holidays and events around the world.</description>
+    <language>en</language>
+    <item>
+      <title>Fridtjof Nansen's 150th Birthday - (Norway)</title>
+      <link>http://www.google.com/intl/en_us/logos/index.html#logo-2011nansen11-hp</link>
+      <description>&lt;a
+      href="http://www.google.com/intl/en_us/logos/index.html#logo-2011nansen11-hp"&gt;&lt;img
+      src="http://www.google.com/logos/2011/nansen11-hp.jpg" alt="Fridtjof Nansen's 150th
+      Birthday"/&gt;&lt;/a&gt;&lt;p&gt;&lt;strong&gt;Date:&lt;/strong&gt; Oct 10,
+      2011&lt;/p&gt;</description>
+    </item>
+    <item>
+      <title>Mihajlo Pupin's 153th Birthday - (Serbia)</title>
+      <link>http://www.google.com/intl/en_us/logos/index.html#logo-2011pupin11-hp</link>
+      <description>&lt;a
+      href="http://www.google.com/intl/en_us/logos/index.html#logo-2011pupin11-hp"&gt;&lt;img
+      src="http://www.google.com/logos/2011/pupin11-hp.jpg" alt="Mihajlo Pupin's 153th
+      Birthday"/&gt;&lt;/a&gt;&lt;p&gt;&lt;strong&gt;Date:&lt;/strong&gt; Oct 09,
+      2011&lt;/p&gt;</description>
+    </item>
+  </channel>
+</rss>
