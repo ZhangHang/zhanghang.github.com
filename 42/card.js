b7c2c10219a2a3748f6079b77c75aa87b4360cb1
Zhang Hang <zh199111@gmail.com>
2013-05-03 23:20:18 +0800

    拆分 card ，根据屏幕自适应填满，新的样式

---
+++ 42/card.js
@@ -0,0 +1,47 @@
+function createCard (row,clounm,parentTable) {
+	for (var i = row - 1; i >= 0; i--) {
+		var tr = document.createElement('tr');
+		
+		for (var j = clounm - 1; j >= 0; j--) {
+			var td = document.createElement('td');
+			var card =  document.createElement('div');
+			
+			card.classList.add('card');
+
+			if(j%2 === 0){
+				card.classList.add('active');
+			}
+
+			td.appendChild(card);
+			tr.appendChild(td);
+		};
+		parentTable.appendChild(tr);
+	};
+}
+
+
+function init(){
+	var cardheight = 90;
+	var cardwidth = 90;
+	var mainTable = document.getElementById('cardContianer');
+	var row = document.height/cardheight;
+	var colunm = document.width/cardwidth;
+	createCard(row,colunm,mainTable);
+}
+
+window.addEventListener("resize", init, false);
+
+
+function tryit(){
+
+	var mainTable = document.getElementById('cardContianer');
+
+	for (var i = mainTable.children.length - 1; i >= 0; i--) {
+		var tr = 	mainTable.children[i];
+
+		for (var j = tr.children.length - 1; j >= 0; j--) {
+			var card = tr.children[j].children[0];
+			card.classList.add('active');
+		};
+	};
+}
